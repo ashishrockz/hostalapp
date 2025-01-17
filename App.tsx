8ea192d58ffe149664971c823ca4a94f9557226c
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2025-01-17 00:43:43 -0800

    the main

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Index from './src/screens/Index'
+
+const App = () => {
+  return (
+    <View>
+      <Index/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
